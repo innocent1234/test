e72b2ea3560c543278ee60cf0e7dbd377a6cfa3c
innocent <innocentbienvenu1@gmail.com>
2019-12-27 18:09:33 +0200

    the first commit added

---
+++ javascript/app.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+    document.body.style.transition="3s";
+    document.body.style.background='gray';
+
+    
+}
+
+
+// form1
+
+const first1=document.getElementById("first1");
+const form1=document.getElementById("form1");
+first1.addEventListener("click", function(){
+    form1.style.display="block"
+})
+//form2
+const first2=document.getElementById("first2");
+const form2=document.getElementById("form2");
+first2.addEventListener("click", function(){
+    form2.style.display="block"
+})
+
